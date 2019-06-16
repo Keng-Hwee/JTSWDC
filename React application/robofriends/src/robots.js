0afc07ac79e766a2c37affc9eb30c2267a1d8e2f
Keng Hwee <kenghwee.t@gmail.com>
2019-06-16 16:33:20 +0800

    Started on react refresher section

---
+++ React application/robofriends/src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: "robot1",
+        username: "user1",
+        email: "test1@test.com"
+    },
+    {
+        id: 2,
+        name: "robot2",
+        username: "user2",
+        email: "test2@test.com"
+    },
+    {
+        id: 3,
+        name: "robot3",
+        username: "user3",
+        email: "test3@test.com"
+    },
+    {
+        id: 4,
+        name: "robot4",
+        username: "user4",
+        email: "test4@test.com"
+    },
+    {
+        id: 5,
+        name: "robot5",
+        username: "user5",
+        email: "test5@test.com"
+    }
+];
